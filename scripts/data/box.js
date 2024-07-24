07f50df9860c93291ef755f0c393b7e8fceddcd6
Ankit_Shome <ankitshome7@gmail.com>
2024-07-24 17:58:33 +0530

    commit change header

--- scripts/data/box.js
+++ scripts/data/box.js
@@ -12,43 +12,68 @@ export const products = [
    {
      id: generateRandomId(10),
      name: 'Home Decor',
-     image: 'images/box1-1.jpg'
+     image: 'images/pic/box1-1.jpg'
    },
    {
      id: generateRandomId(10),
      name: 'Shop activity trackers and smartwatches',
-     image: 'images/box1-2.jpg'
+     image: 'images/pic/box1-2.jpg'
    },
    {
      id: generateRandomId(10),
      name: 'Toy under $25',
-     image: 'images/box1-3.jpg'
+     image: 'images/pic/box1-3.jpg'
    },
    {
      id: generateRandomId(10),
      name: 'Deals in PCs',
-     image: 'images/box1-4.jpg'
+     image: 'images/pic/box1-4.jpg'
    },
 
    {
       id: generateRandomId(10),
       name: 'Grooming Products',
-      image: 'images/box2-1.jpg'
+      image: 'images/pic/box2-1.jpg'
     },
     {
       id: generateRandomId(10),
       name: 'Latest Devices',
-      image: 'images/box2-2.jpg'
+      image: 'images/pic/box2-2.jpg'
     },
     {
       id: generateRandomId(10),
       name: 'Pets Food',
-      image: 'images/box2-3.jpg'
+      image: 'images/pic/box2-3.jpg'
     },
     {
       id: generateRandomId(10),
       name: 'Fashion Mart',
-      image: 'images/box2-4.jpg'
+      image: 'images/pic/box2-4.jpg'
     },
+
+    {
+      id: generateRandomId(10),
+      name: 'Stationary',
+      image: 'images/pic/box3-1.jpg'
+    },
+
+    {
+      id: generateRandomId(10),
+      name: 'Laptops',
+      image: 'images/pic/box3-2.jpg'
+    },
+
+    {
+      id: generateRandomId(10),
+      name: 'Study Chairs',
+      image: 'images/pic/box3-3.jpg'
+    },
+
+    {
+      id: generateRandomId(10),
+      name: 'Monitors',
+      image: 'images/pic/box3-4.jpg'
+    },
+
     
  ];
